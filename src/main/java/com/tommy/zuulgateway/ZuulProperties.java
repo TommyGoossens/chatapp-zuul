2b8354eb0171ba3f690fc1ebd461702bd566f96f
Tommy Goossens <tommygoossens@ziggo.nl>
2019-05-22 23:12:42 +0200

    Setup Zuul

---
+++ src/main/java/com/tommy/zuulgateway/ZuulProperties.java
@@ -0,0 +1,4 @@
+package com.tommy.zuulgateway;
+
+public class ZuulProperties {
+}
---
+++ src/main/java/com/tommy/zuulgateway/ZuulgatewayApplication.java
@@ -0,0 +1,22 @@
+package com.tommy.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulgatewayApplication.class, args);
+    }
+
+}
